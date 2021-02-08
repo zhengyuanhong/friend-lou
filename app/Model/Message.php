22c6961c9404fbdd377b64f8eccc332d210f0daa
zhengyuanhong <1713639570@qq.com>
2021-02-08 22:41:18 +0800

    '消息通知添加软删除'

--- app/Model/Message.php
+++ app/Model/Message.php
@@ -2,11 +2,12 @@
 
 namespace App\Model;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
+    use SoftDeletes;
     protected $table = 'message';
 
     public static $statusMap = [
