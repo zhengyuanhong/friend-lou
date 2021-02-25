e73a10b963c31d59d7eab14143461969a56737a1
zhengyuanhong <1713639570@qq.com>
2021-02-25 23:16:36 +0800

    fix

--- app/Console/Commands/cron/RepaymentNotify.php
+++ app/Console/Commands/cron/RepaymentNotify.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\cron;
 
+use App\Jobs\sendMessage;
 use App\Utils\Wechat\NotifyMessage;
 use App\Utils\Wechat\RepaymentMessage;
 use Carbon\Carbon;
@@ -41,18 +42,19 @@ class RepaymentNotify extends Command
      */
     public function handle()
     {
-        $notify = new NotifyMessage();
-        $notify->setPage('/pages/index/index');
-        //留言通知
-        $notify->setTemplateId('YtyUhxysvRH-ESC0oiE6CKlKz5tFqS5LtQ801TsTT4k');
-        //账单通知
-//        $notify->setTemplateId('6agnykuZddRbPjnMSWrZD0iecg32D7kWaMYmD8bOmho');
-//        $notify->setTemplateId('6agnykuZddRbPjnMSWrZD0iecg32D7kWaMYmD8bOmho');
-        $notify->setToUser('oiVhr5XX-NjY0K6WN8CapXHLmJrw');
-        $notify->setData(400,'2020-1-12','test',Carbon::now());
-        $data = $notify->getData();
-        $app = app('easyWechat');
-        $app->subscribe_message->send($data);
-        Log::info('repayment_notify:',$data);
+//        $notify = new NotifyMessage();
+//        $notify->setPage('/pages/index/index');
+//        //留言通知
+//        $notify->setTemplateId('YtyUhxysvRH-ESC0oiE6CKlKz5tFqS5LtQ801TsTT4k');
+//        //账单通知
+////        $notify->setTemplateId('6agnykuZddRbPjnMSWrZD0iecg32D7kWaMYmD8bOmho');
+////        $notify->setTemplateId('6agnykuZddRbPjnMSWrZD0iecg32D7kWaMYmD8bOmho');
+//        $notify->setToUser('oiVhr5XX-NjY0K6WN8CapXHLmJrw');
+//        $notify->setData(400,'2020-1-12','test',Carbon::now());
+//        $data = $notify->getData();
+//        $app = app('easyWechat');
+//        $app->subscribe_message->send($data);
+//        Log::info('repayment_notify:',$data);
+        sendMessage::dispatch();
     }
 }
